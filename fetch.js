1f0a77cc7771f18d72bcc903bd3303c8c8d8cff8
Dhairya Jain <31620568+dhairyaj@users.noreply.github.com>
2020-01-17 16:09:17 +0530

    to get the details

---
+++ fetch.js
@@ -0,0 +1,7 @@
+var name = document.getElementById("name");
+var number = document.getElementById("number");
+var symbol = document.getElementById("symbol");
+var atomic_mass = document.getElementById("atomic_mass");
+var category = document.getElementById("category");
+var phase = document.getElementById("phase");
+var color = document.getElementById("color");
