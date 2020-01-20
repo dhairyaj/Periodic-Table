a8550bcbc0ac4df889540fb1d93223dcb57b8c67
Samarth175 <samarth1751997@gmail.com>
2020-01-20 05:50:53 -0500

    added fetch logic to frontend

--- public/js/fetch.js
+++ public/js/fetch.js
@@ -1,7 +1,29 @@
-var name = document.getElementById("name");
-var number = document.getElementById("number");
-var symbol = document.getElementById("symbol");
-var atomic_mass = document.getElementById("atomic_mass");
-var category = document.getElementById("category");
-var phase = document.getElementById("phase");
-var color = document.getElementById("color");
+const name = document.getElementById("name");
+const atomicNum = document.getElementById("atomicNum");
+const symbol = document.getElementById("symbol");
+const atomic_mass = document.getElementById("atomic_mass");
+const category = document.getElementById("category");
+const phase = document.getElementById("phase");
+
+function submitVal(){
+    var number = document.getElementById('number').value;
+    fetch('http://localhost:3000/elements/'+number).then((response) => {
+        response.json().then((data) => {
+            if(data.error){
+                alert("Something went wrong!")
+                // console.log(data.error)
+            }
+            else{
+                name.innerHTML = data.name
+                atomicNum.innerHTML = data.number
+                symbol.innerHTML = data.symbol
+                atomic_mass.innerHTML = data.atomic_mass
+                category.innerHTML = data.category
+                phase.innerHTML = data.phase
+            }
+        });
+    }).catch((error) => {
+        alert("Something went wrong!")
+        // console.log(error)
+    })
+}
